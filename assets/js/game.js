252e5f878e54d4adf33cb17330150df662423813
valcomp <valerie.aleida.lopez@gmail.com>
2022-04-15 01:27:04 -0500

    adding alerts

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a meessage");
+//this will do math and log 20
+//what is this?
+console .log ("Our robot's name is " + playername);
+
+// this creates a function named "fight"
+function fight (){
+    window.alert("The fight has begun!");
+}
+
+//fight();

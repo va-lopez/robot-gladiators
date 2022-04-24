7ef806044c1d9c159a20d9ecabfe7557d1742804
valcomp <valerie.aleida.lopez@gmail.com>
2022-04-24 16:36:34 -0500

    fixed empty name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,7 +78,7 @@ var startGame = function(){
 
         if(playerInfo.health>0){
             window.alert("Welcome to Robot Gladiators! Round " + (i +1));
-    
+            
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40,60);
         
@@ -164,8 +164,22 @@ var shop = function(){
     }
 };
 
+//function to set name
+
+var getPlayerName = function(){
+    var name = "";
+
+while( name === "" || name === null){
+    name = prompt("What is your robot's name?");
+}
+
+console.log("Your robot's name is " + name);
+return name;
+};
+
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
